da54c7a802f0e674f78424d62e2eb6205069f219
Kimiya <102611881+Kimiagoudarzi@users.noreply.github.com>
2022-06-24 10:00:35 +0430

    handle edit form.html kimia goudarzi

--- app.js
+++ app.js
@@ -19,7 +19,8 @@ const state = document.getElementById("userState");
 const street = document.getElementById("userStreet");
 const No = document.getElementById("userNo");
 const unit = document.getElementById("userUnit");
-const dlt = document.getElementById("deleteItem")
+const dlt = document.getElementById("deleteItem");
+const addBtn = document.getElementById("submit");
 document.addEventListener("DOMContentLoaded", () => {
   readUsers();
 });
@@ -55,10 +56,10 @@ function addToDom(items) {
                             <td>${items.no}</td>
                             <td>${items.floor}</td>
                             <td>${items.unit}</td>
-                            <td><svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
-                                <path d="M17.414 2.586a2 2 0 00-2.828 0L7 10.172V13h2.828l7.586-7.586a2 2 0 000-2.828z" />
-                                <path fill-rule="evenodd" d="M2 6a2 2 0 012-2h4a1 1 0 010 2H4v10h10v-4a1 1 0 112 0v4a2 2 0 01-2 2H4a2 2 0 01-2-2V6z" clip-rule="evenodd" />
-                              </svg>
+                            <td>
+                            <button onclick="editInfo(${items.id})" type="button" class="btn btn-primary sm ">
+                            edit
+                          </button>
                             </td>
                             <td>
                             <button onclick="modalInfo(${items.id})" type="button" class="btn btn-danger sm " data-bs-toggle="modal" data-bs-target="#exampleModal">
@@ -158,3 +159,13 @@ dlt.addEventListener('click', (e)=>{
   })
   
 })
+
+
+addBtn.addEventListener("click", (e)=>{
+  window.location.href="/form.html";
+  
+})
+
+function editInfo(id){
+  window.location.href =`/form.html?id=${id}`;
+}
